d4390d6c26af40a98648760eaab8034242ec6a0c
unknown <User@.(none)>
2017-01-27 13:37:53 +0200

    first commit

---
+++ wp-content/themes/twentyseventeen/page-featured-films.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php 
+			
+			// args
+			$args = array(
+				'numberposts'	=> -1,
+				'post_type'		=> 'films',
+				'meta_key'		=> 'featured',
+				'meta_value'	=> '1'
+			);
+			
+			
+			// query
+			$the_query = new WP_Query( $args );
+			
+			//vd($the_query);
+			//vd($the_query->have_posts());
+			?>
+			
+			
+			
+			
+			
+		<?php
+		if ( $the_query->have_posts() ) : ?>
+			<?php
+			/* Start the Loop */
+			while ( $the_query->have_posts() ) : $the_query->the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/post/content', get_post_format() );
+
+			endwhile;
+
+			the_posts_pagination( array(
+				'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+				'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+			) );
+
+		else :
+
+			get_template_part( 'template-parts/post/content', 'none' );
+
+		endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
